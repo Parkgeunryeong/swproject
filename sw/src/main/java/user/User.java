5535f3a3192b068258b1cb71a496b61c5999a87e
kkgk7 <kkgk7@Park>
2024-10-06 13:54:37 +0900

    swprojext

---
+++ sw/src/main/java/user/User.java
@@ -0,0 +1,52 @@
+package user;
+
+import java.math.BigDecimal;
+
+public class User {
+	private String userID;
+	private String userPassword;
+	private String userName;
+	private BigDecimal latitude;
+	private BigDecimal longitude;
+	private String email;
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public BigDecimal getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(BigDecimal latitude) {
+		this.latitude = latitude;
+	}
+	public BigDecimal getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(BigDecimal longitude) {
+		this.longitude = longitude;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+
+}
